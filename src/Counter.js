66673bcf2eafbbfb62b88ff4a3c4f360a39c242b
egoing <egoing@gmail.com>
2024-08-19 17:41:33 +0900

    리액트에서 사이드 이팩트란 외부와 통신하거나, 브라우저 저장하는 즉 결과를 예측하기 어려운 작업들을 의미한다.
    사이드 이팩트는 useEffect 안에 격리한다.
    
    useEffect의 첫번재 인자는 함수다. 두번째 인자는 배열이다.
    
    함수 안에 사이드 이팩트를 격리한다.
    
    두번째 파라미터가 없으면 컴포넌트가 랜더링 될 때마다 첫번째 함수가 실행된다.
    
    원소가 없으면 단한번 실행된다.
    
    원소가 있으면 원소의 값이 바뀔 때만 실행된다.

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,19 @@
 import {Button, ButtonGroup} from '@mui/material';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 export function Counter({title, initValue}){
     const [value, setValue] = useState(initValue);
+
+      // http://localhost:9999/counter에서 데이터를 가져오고
+      // 그 데이터는 json 타입이니까 파싱을 해서 js의 데이터로 변환하고
+      // 그 결과 값인 value를 setValue로 설정한다.
+      // 위의 코드는 단한번 실행되어야 한다.  
+
+      useEffect(()=>{
+        fetch('http://localhost:9999/counter')
+        .then(resp=>resp.json())
+        .then(result=>setValue(result.value));
+      }, [])
+
       function reset(){
         setValue(0);
       }
@@ -29,7 +41,3 @@ export function Counter({title, initValue}){
         </div>
       )
     }
-
-export function Hi(){
-    return <div>HI</div>
-}
