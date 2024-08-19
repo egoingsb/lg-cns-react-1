e255e8b0f7d6b356d5e6f635056ba6106cdf59df
egoing <egoing@gmail.com>
2024-08-19 11:02:18 +0900

    리턴값을 괄호로 묶으면 더 안전해진다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 function Counter(){
-  return <div>
-    <h1>Counter</h1>
-    <button>+</button> 10
-  </div>
+  return (
+    <div>
+      <h1>Counter</h1>
+      <button>+</button> 10
+    </div>
+  )
 }
 
 function App() {
