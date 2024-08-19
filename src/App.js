10fd7ad12b9de6ff3e6165e58b95efed45b12e14
egoing <egoing@gmail.com>
2024-08-19 16:25:39 +0900

    다이얼로그 구현 - 이벤트 붙이고, - 상태 만들고, open props에 상태를 연결한다. - 이벤트가 발생했을 때 상태를 변경한다.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import { Button, ButtonGroup, Container } from '@mui/material';
+import { Button, ButtonGroup, Container, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 
 function Counter({title, initValue}){
 const [value, setValue] = useState(initValue);
@@ -33,9 +33,26 @@ const [value, setValue] = useState(initValue);
 }
 
 function App() {
+  const [dialogOpen, setDialogOpen] = useState(false);
   return (
     <Container maxWidth="md">
       <Counter title="카운터" initValue={20}></Counter>
+
+      <Dialog open={dialogOpen} onClose={()=>{setDialogOpen(false)}}>
+        <DialogTitle>카운터</DialogTitle>
+        <DialogContent>
+          <Counter title="다이얼로그 카운터" initValue={10}></Counter>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={()=>{
+            setDialogOpen(false);
+          }}>닫기</Button>
+        </DialogActions>
+      </Dialog>
+
+      <Button onClick={()=>{
+        setDialogOpen(true);
+      }} variant="contained">Open Counter</Button>
     </Container>
   );
 }
