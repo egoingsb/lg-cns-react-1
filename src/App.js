41eef0d430c4b6309126c0564dda8e73e11ecc2a
egoing <egoing@gmail.com>
2024-08-19 17:28:09 +0900

    dialog 제거

--- src/App.js
+++ src/App.js
@@ -3,26 +3,9 @@ import './App.css';
 import { Button, ButtonGroup, Container, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 import { Counter } from './Counter';
 function App() {
-  const [dialogOpen, setDialogOpen] = useState(false);
   return (
     <Container maxWidth="md">
       <Counter title="카운터" initValue={20}></Counter>
-
-      <Dialog open={dialogOpen} onClose={()=>{setDialogOpen(false)}}>
-        <DialogTitle>카운터</DialogTitle>
-        <DialogContent>
-          <Counter title="다이얼로그 카운터" initValue={10}></Counter>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={()=>{
-            setDialogOpen(false);
-          }}>닫기</Button>
-        </DialogActions>
-      </Dialog>
-
-      <Button onClick={()=>{
-        setDialogOpen(true);
-      }} variant="contained">Open Counter</Button>
     </Container>
   );
 }
