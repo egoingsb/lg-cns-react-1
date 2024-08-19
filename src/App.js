eada35fcbb0b2b63441b7c66a148aedd91420815
egoing <egoing@gmail.com>
2024-08-19 14:00:33 +0900

    set 함수를 호출 할 때 파라미터에 따라서 입력 값을 주의 깊게 선택해야 한다.
    1. 값이 고정 값인 경우 (reset) -> 인자로 값을 준다.
    2. 이전 값을 기반으로 새로운 값을 주입하는 경우 : 인자로 값을 주면 (up) 마지막 값만 반영되서 버그가 생길 수 있다. 이런 경우 콜백 함수를 입력한다.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import { useState } from 'react';
 import './App.css';
 
 function Counter({title, initValue}){
-  const [value, setValue] = useState(initValue);
-  function up(){
-    setValue(value + 1)
+const [value, setValue] = useState(initValue);
+  function reset(){
+    setValue(0);
   }
+  // 꼭 이해해야 하는 부분
+  function up(){
+    setValue(value + 1);
+  }
+  // 하면 좋고, 안해도 왠만하면 괜찮은 부분
+  function down(){
+    function callback(prev){
+      console.log('prev', prev);
+      return prev - 1;
+    }
+    setValue(callback);
+  }
+
   return (
     <div>
       <h1>{title}</h1>
-      <button onClick={up}>+</button> {value}
+      <button onClick={up}>+</button> 
+      <button onClick={down}>-</button> 
+      <button onClick={reset}>0</button>
+      {value}
     </div>
   )
 }
