3a9b8eebf6ad1605c7b181e9b72a368c7e9e6273
egoing <egoing@gmail.com>
2024-08-19 11:48:37 +0900

    이벤트는 함수다. useState는 기본 값을 바탕으로 상태를 만든다. state는 배열이다. 첫번째 원소는 읽을 때, 두번째 원소는 수정할 때 사용하는 함수다. state의 값이 바뀌면 컴포넌트가 다시 실행되고, 바뀐 값이 새로운 state의 값이 된다.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import './App.css';
 
 function Counter({title, initValue}){
+  // let value = initValue;
+  let valueState = useState(initValue);
+  let value = valueState[0];
+  let setValue = valueState[1];
+  function up(){
+    setValue(value + 1)
+  }
   return (
     <div>
       <h1>{title}</h1>
-      <button>+</button> {initValue}
+      <button onClick={up}>+</button> {value}
     </div>
   )
 }
@@ -13,6 +21,7 @@ function App() {
   return (
     <div>
       <Counter title="카운터" initValue={20}></Counter>
+      <Counter title="불면증 카운터" initValue={40}></Counter>
     </div>
   );
 }
