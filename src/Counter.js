ddf488cd18f9db8085ab42a77d2a497ba67ec78c
egoing <egoing@gmail.com>
2024-08-20 11:58:54 +0900

    태그들을 원소로 하는 배열을 출력하면 자동으로 태그가 출력된다.
    배열의 원소를 수정해서 새로운 배열을 만들 때는 map 함수를 사용한다.
    
    각 원소는 key값을 가져야 한다. key값은 목록 안에서 고유해야 한다.
    
    객체, 배열인 상태를 변경할 때는 원본을 복제하고, 복제본을 수정한다.
    
    배열 복제 :  [...원본]
    객체 복제 : {...원본}

--- src/Counter.js
+++ src/Counter.js
@@ -3,6 +3,10 @@ import {useEffect, useState} from 'react';
 export function Counter({title, initValue}){
     const [value, setValue] = useState(initValue);
     const [step, setStep] = useState(1);
+    const [history, setHistory] = useState([
+      {value:1, time: '2021-10-01'},
+      {value:2, time: '2021-10-02'},
+    ])
       async function fetchCounter(){
         const resp = await fetch('http://localhost:9999/counter');
         const result = await resp.json();
@@ -12,17 +16,30 @@ export function Counter({title, initValue}){
         fetchCounter();
       }, [])
       async function up(){
+        const newValue = value + step;
         const option = {
           method: 'PUT',
           headers: {
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify({value: value+step})
+          body: JSON.stringify({value: newValue})
         }
         const resp = await fetch('http://localhost:9999/counter',option);
         await resp.json();
         await fetchCounter();
+
+        // 원본을 수정하면 안됩니다. 
+        // history.push({value: newValue, time: '2021-10-03'});
+        // setHistory(history);
+
+        // 복제본을 수정해야 합니다. 
+        const newHistory = [...history];
+        newHistory.push({value: newValue, time: new Date().toLocaleString()});
+        setHistory(newHistory);
       }
+      const historyTag = history.map((item, index)=>{
+        return <li key={index}>{item.value}, {item.time}</li>
+      })
       return (
         <div>
           <h1 className="heading1">{title}</h1>
@@ -33,6 +50,9 @@ export function Counter({title, initValue}){
             <Button onClick={up}>+</Button>
           </ButtonGroup>
           {value}
+          <ul>
+            {historyTag}
+          </ul>
         </div>
       )
     }
