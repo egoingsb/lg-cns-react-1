dd8389186e9ccb1874980998ea916d88ea3f81a0
egoing <egoing@gmail.com>
2024-08-20 10:29:10 +0900

    코드 정리

--- src/Counter.js
+++ src/Counter.js
@@ -8,38 +8,20 @@ export function Counter({title, initValue}){
         setValue(result.value);
       }
       useEffect(()=>{
-        // fetch('http://localhost:9999/counter')
-        // .then(resp=>{
-        //   return resp.json()
-        // })
-        // .then(result=>{
-        //   setValue(result.value)
-        // });
         fetchCounter();
       }, [])
-
-      function reset(){
-        setValue(0);
-      }
       function up(){
         setValue((prev) => prev+1);
       }
-      function down(){
-        setValue((prev) => prev-1);
-      }
-    
       const inlineStyle = {
         color:'blue',
         padding: '10px'
       }
-    
       return (
         <div>
           <h1 className="heading1">{title}</h1>
           <ButtonGroup variant="contained">
             <Button onClick={up}>+</Button>
-            <Button onClick={down}>-</Button>
-            <Button onClick={reset}>0</Button>
           </ButtonGroup>
           {value}
         </div>
